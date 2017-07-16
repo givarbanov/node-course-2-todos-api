bee3e0d7e90975b3fe581fff97244c433c123791
givarbanov <givarbanov@gmail.com>
2017-07-16 11:50:21 +0300

    Change port

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -46,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Start and listening on port 3000');
+app.listen(port, () => {
+  console.log(`Start and listening on port ${port}`);
 });
 
 module.exports = {app};
