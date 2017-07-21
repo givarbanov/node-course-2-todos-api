3afc52e518e1df887fe46eeff1b192f5d92fbabf
Georgi Varbanov <georgi.varbanov@hds-group.local>
2017-07-21 11:13:55 +0300

    Add test for DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -122,7 +122,6 @@ app.post('/users/login', (req, res) => {
 });
 
 app.delete('/users/me/token', authenticate, (req, res) => {
-  console.log(req);
   req.user.removeToken(req.token).then(() => {
     res.status(200).send();
   }, () => {
