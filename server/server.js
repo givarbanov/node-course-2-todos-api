77bd4a0d29122b91b875a1aecf885ff0b1a400a2
Georgi Varbanov <georgi.varbanov@hds-group.local>
2017-07-24 10:39:21 +0300

    Modifications

--- server/server.js
+++ server/server.js
@@ -125,9 +125,9 @@ app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
   User.findByCredential(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
-    });
+    return user.generateAuthToken();
+  }).then(({token, user}) => {
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send();
   });
